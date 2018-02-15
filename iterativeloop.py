2059f45e5af583b59a4929243a40bf4ab863ab69
Chase Turner <chase.turner@mckesson.com>
2018-02-14 19:57:46 -0500

    Final solution with comments

--- iterativeloop.py
+++ iterativeloop.py
@@ -1,7 +1,7 @@
-#Write a loop that adds the numbers 1 through 50.
-#At some point the total value will be greater than 100.
-#Have the loop print the number that makes the total
-#greater than 100 and print the message "The sum exceeded the max value of 100."
+# Write a loop that adds the numbers 1 through 50.
+# At some point the total value will be greater than 100.
+# Have the loop print the number that makes the total
+# greater than 100 and print the message "The sum exceeded the max value of 100."
 
 x = 1
 total = 0
@@ -14,4 +14,6 @@ while x <= 51:
   total += x
   x += 1
 
-print(x)
+print('The interation by which the total of previous in the series completed the jump past the limit asked is ' ,x-1) # The reason this is - 1 is because the trigger was set once the count
+           # went past 100 and explicitly added 1, which is then printed. The actual
+           # solution is a result of line 15 being reduced by 1.
